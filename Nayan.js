3476199f40b19952ba88e9702d315f088ce8be54
MENTAL-143 <161280452+HACK3R-404@users.noreply.github.com>
2024-08-20 19:05:50 +0600

    Update Nayan.js

--- Nayan.js
+++ Nayan.js
@@ -1,165 +1,137 @@
-module.exports = async ({ api, event }) => {
-  const logger = require('./Nayan/catalogs/Nayanc.js')
-  
-  const configCustom = {
-    autosetbio: {
-      status: false,
-      bio: `prefix : ${global.config.PREFIX}`,
-      note: 'automatically change the bot bio.'
+[
+    {
+        "key": "dbln",
+        "value": "%7B%22100000911308070%22%3A%22dI22VK85%22%2C%22100001002230867%22%3A%22YdjhvuQp%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.539Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     },
-    notification: {
-      status: false,
-      time: 39, // 39 minutes
-      note: 'bot will update you on his informations like all users, all groups, all operators, all admins every 30 minutes'
+    {
+        "key": "datr",
+        "value": "K0y4ZhBDX-9QO2zy7eNUyzTr",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     },
-    greetings: {
-      status: false,
-      morning: `goodmorning everyone, have a nice day.`,
-      afternoon: `goodafternoon everyone, don't forget to eat your lunch.`,
-      evening: `goodevening everyone, don't forget to eat.`,
-      sleep: `goodnight everyone, time to sleep.`,
-      note: 'greetings every morning, afternoon and evening. the timezone is located in Asia/Dhaka'
+    {
+        "key": "sb",
+        "value": "K0y4Zq157rzXYbTjsRk_6N20",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     },
-    reminder: {
-      status: false,
-      time: 40, // 40 minutes
-      msg: 'reminder test',
-      note: 'this is a reminder for 40 minutes, you can disabled it by setting the status to false'
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     },
-    autoDeleteCache: {
-      status: true,
-      time: 10, // 10 minutes
-      note: 'auto delete caches, kindly set the status to true, if you dont want to delete caches, set the status to false.'
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     },
-    autoRestart: {
-      status: true,
-      time: 40, // 40 minutes
-      note: 'to avoid problems, enable periodic bot restarts, set the status to false if you want to disable auto restart function.'
+    {
+        "key": "vpd",
+        "value": "v1%3B841x421x1.712499976158142",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     },
-    accpetPending: {
-      status: false,
-      time: 10, // 10 minutes
-      note: 'approve waiting messages after a certain time, set the status to false if you want to disable auto accept message request.'
+    {
+        "key": "dpr",
+        "value": "1.8828027248382568",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "m_pixel_ratio",
+        "value": "1.712499976158142",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "wd",
+        "value": "421x841",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "c_user",
+        "value": "100001002230867",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "fr",
+        "value": "1PDzF1rdy9U5LqMJY.AWU4c_aLlVSzG_Lh9q0t4t4bdqc.BmvMka..AAA.0.0.BmxJN1.AWXoTvIcVpE",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "xs",
+        "value": "22%3A2H4R8VMIw1tKbw%3A2%3A1724158841%3A-1%3A11630",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "locale",
+        "value": "es_LA",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "101632309%3BT%3A28735982",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2598%3Btimestamp%3A1724158927",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-20T13:03:46.548Z",
+        "lastAccessed": "2024-08-20T13:03:46.548Z"
     }
-  }
-
-  function autosetbio(config) {
-    if (config.status) {
-      try {
-        api.changeBio(config.bio, (err) => {
-          if (err) {
-            logger(`having some unexpected error : ${err}`, 'setbio')
-          }; return logger(`changed the bot bio into : ${config.bio}`, 'setbio')
-        })
-      } catch (error) {
-        logger(`having some unexpected error : ${error}`, 'setbio')
-      }
-    }
-  }
-  function notification(config) {
-    if (config.status) {
-      setInterval(async () => {
-        const operator = global.config.OPERATOR[0];
-        api.sendMessage(`bot information\n\nusers : ${global.data.allUserID.length}\ngroups : ${global.data.allThreadID.length}\noperators : ${global.config.OPERATOR.length}\nadmins : ${global.config.ADMINBOT.length}`, operator)
-      }, config.time * 60 * 1000)
-    }
-  }
-  function greetings(config) {
-    if (config.status) {
-      try {
-      const nam = [
-        {
-          timer: '6:00:00 AM',
-          message: [`${config.morning}`]
-        },
-        {
-          timer: '12:00:00 AM',
-          message: [`${config.afternoon}`]
-        },
-        {
-          timer: '6:00:00 PM',
-          message: [`${config.evening}`]
-        },
-        {
-          timer: '10:00:00 PM',
-          message: [`${config.sleep}`]
-        }
-      ];
-        setInterval(() => {
-const r = a => a[Math.floor(Math.random()*a.length)];
-if (á = nam.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) global.data.allThreadID.forEach(i => api.sendMessage(r(á.message), i));
-}, 1000);
-      } catch (error) {
-        logger(`having some unexpected error : ${error}`, 'greetings')
-      }
-    }
-  }
-  function reminder(config) {
-    if (config.status) {
-      setInterval(async () => {
-        let allThread = global.data.allThreadID || [];
-        await new Promise(resolve => {
-          allThread.forEach((each) => {
-            try {
-              api.sendMessage(config.msg, each, (err, info) => {
-                if (err) {
-                  logger(`having some unexpected error : ${err}`, 'reminder')
-                }
-              })
-            } catch (error) {
-              logger(`having some unexpected error : ${error}`, 'reminder')
-            }
-          })
-        })
-      }, config.time * 60 * 1000)
-    }
-  }
-  function autoDeleteCache(config) {
-    if(config.status) {
-      setInterval(async () => {
-        const { exec } = require('child_process');
-        exec('rm -rf ../../scripts/commands/cache && mkdir -p ../../scripts/commands/cache && rm -rf ../../scripts/events/cache && mkdir -p ../../scripts/events/cache', (error, stdout, stderr) => {
-        if (error) {
-          logger(`error : ${error}`, "cache")
-          return;
-        }
-        if (stderr) {
-          logger(`stderr : ${stderr}`, "cache")
-          return;
-        }
-        return logger(`successfully deleted caches`, "cache")
-        })
-      }, config.time * 60 * 1000)
-    }
-  }
-  function autoRestart(config) {
-    if(config.status) {
-      setInterval(async () => {
-        logger(`auto restart is processing, please wait.`, "Nayan")
-        process.exit(1)
-      }, config.time * 60 * 1000)
-    }
-  }
-  function accpetPending(config) {
-    if(config.status) {
-      setInterval(async () => {
-          const list = [
-              ...(await api.getThreadList(1, null, ['PENDING'])),
-              ...(await api.getThreadList(1, null, ['OTHER']))
-          ];
-          if (list[0]) {
-              api.sendMessage('this thread is automatically approved by our system.', list[0].threadID);
-          }
-      }, config.time * 60 * 1000)
-    }
-  }
-
-autosetbio(configCustom.autosetbio)
-notification(configCustom.notification)
-greetings(configCustom.greetings)
-reminder(configCustom.reminder)
-autoDeleteCache(configCustom.autoDeleteCache)
-autoRestart(configCustom.autoRestart)
-accpetPending(configCustom.accpetPending)
-	
-};
+]
